297d4acd81c6a3cfe3e21244e8c89045e84b0b79
dumkaguv <golovichkind@gmail.com>
2025-02-09 17:28:17 +0200

    add: builded project for

--- dist/assets/NotFound-CF4iAIB6.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-DhzqFn7b.js";const e=()=>s.jsxs("div",{className:"container",children:[s.jsx("h1",{children:"404"}),s.jsx("p",{children:"Страница не найдена"})]});export{e as default};
---
+++ dist/assets/NotFound-uSkVD7eG.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-jBBoQFAi.js";const e=()=>s.jsxs("div",{className:"container",children:[s.jsx("h1",{children:"404"}),s.jsx("p",{children:"Страница не найдена"})]});export{e as default};
